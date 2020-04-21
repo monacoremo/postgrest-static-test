2d0f0fa7b680c92a60ea2d317719e7e53952b0ca
Remo <59358383+monacoremo@users.noreply.github.com>
2020-04-22 00:26:16 +0200

    Update haskell-packages.nix

--- overlays/haskell-packages.nix
+++ overlays/haskell-packages.nix
@@ -1,18 +1,16 @@
-{ compiler }:
-
 self: super:
+let
+  unbreak =
+    drv:
+      self.haskell.lib.overrideCabal drv (drv: { broken = false; });
+
+  dontCheck =
+    self.haskell.lib.dontCheck;
+in
 {
   haskellPackages =
-    super.haskell.packages."${compiler}".override {
+    super.haskellPackages.override {
       overrides =
-        let
-          unbreak =
-            drv:
-              self.haskell.lib.overrideCabal drv (drv: { broken = false; });
-
-          dontCheck =
-            self.haskell.lib.dontCheck;
-        in
         newPkgs: oldPkgs: rec {
           hasql-pool = dontCheck (unbreak oldPkgs.hasql-pool);
           configurator-pg = dontCheck (unbreak oldPkgs.configurator-pg);
