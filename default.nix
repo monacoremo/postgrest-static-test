be8846e3c3cdbd4b3e1e0ef2310878a371ef0f39
Remo <59358383+monacoremo@users.noreply.github.com>
2020-04-21 23:42:04 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,29 @@
 let
+  nixpkgsVersion =
+    {
+      date = "2020-04-19";
+      rev = "10100a97c8964e82b30f180fda41ade8e6f69e41";
+      tarballHash = "011f36kr3c1ria7rag7px26bh73d1b0xpqadd149bysf4hg17rln";
+    };
+
+  pinnedPkgs =
+    builtins.fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsVersion.rev}.tar.gz";
+      sha256 = nixpkgsVersion.tarballHash;
+    };
+
   pkgs =
-    ...;
+    import pinnedPkgs {};
 
   static-haskell-nix =
-    ...;
+    fetchTarball https://github.com/nh2/static-haskell-nix/archive/d1b20f35ec7d3761e59bd323bbe0cca23b3dfc82.tar.gz;
 
   survey =
     import "${static-haskell-nix}/survey"
       {
         normalPackages = pkgs;
         #compiler = "ghc883";
-        #cabal...
+        #defaultCabalPackageVersionComingWithGhc = "Cabal_3_2_0_0";
         #integer-simple = true;
       };
 in
